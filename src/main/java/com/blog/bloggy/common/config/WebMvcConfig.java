cbd9885ee1b0da256dfcbb7a66b040496fd11369
이효곤 <gon109m@gmail.com>
2023-06-06 20:01:33 +0900

    Feat: ec2 gitaction 테스트

--- src/main/java/com/blog/bloggy/common/config/WebMvcConfig.java
+++ src/main/java/com/blog/bloggy/common/config/WebMvcConfig.java
@@ -17,7 +17,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(accessInterceptor)
-                .excludePathPatterns("/home","/users","/login", "/error","/v1/auth/refresh");
+                .excludePathPatterns("/home","/users","/test",
+                        "/login", "/error","/v1/auth/refresh");
         registry.addInterceptor(refreshInterceptor)
                 .addPathPatterns("/v1/auth/refresh");
     }
--- src/main/java/com/blog/bloggy/common/controller/HomeController.java
+++ src/main/java/com/blog/bloggy/common/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.blog.bloggy.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -10,6 +11,24 @@ public class HomeController {
     @GetMapping(value="/home")
     public String hello() {
         return "home";
+    }
+
+    @ResponseBody
+    @GetMapping(value="/test")
+    public Test test(){
+        Test test=new Test();
+        test.setName("테스트 성공");
+        return test;
+    }
+    public class Test{
+        String name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
