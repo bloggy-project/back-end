141cfa7c064a6273493fa5cd9234730dba640c4f
이효곤 <gon109m@gmail.com>
2023-06-14 15:40:42 +0900

    Refactor: Favorite 예외 처리 리팩토링

--- src/main/java/com/blog/bloggy/favorite/service/FavoriteService.java
+++ src/main/java/com/blog/bloggy/favorite/service/FavoriteService.java
@@ -1,11 +1,11 @@
 package com.blog.bloggy.favorite.service;
 
+import com.blog.bloggy.common.exception.*;
 import com.blog.bloggy.favorite.model.Favorite;
 import com.blog.bloggy.post.model.Post;
 import com.blog.bloggy.user.model.UserEntity;
 import com.blog.bloggy.favorite.dto.FavoriteDto;
 import com.blog.bloggy.favorite.dto.ResponseFavoriteClick;
-import com.blog.bloggy.common.exception.NotFoundException;
 import com.blog.bloggy.favorite.repository.FavoriteRepository;
 import com.blog.bloggy.post.repository.PostRepository;
 import com.blog.bloggy.user.repository.UserRepository;
@@ -21,28 +21,16 @@ public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
-    public ResponseFavoriteClick createFavorite(FavoriteDto favoriteDto) {
-        Long postId = favoriteDto.getPostId();
-        String userId = favoriteDto.getUserId();
-        Post post = postRepository.findByIdWithUserComment(postId).orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다"));
-        UserEntity user = userRepository.findByUserId(userId).orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다."));;
-        Favorite favorite = new Favorite();
-        favorite.setAddFavorite(post);
-        favoriteRepository.save(favorite);
-        return ResponseFavoriteClick.builder()
-                .postId(postId)
-                .userId(userId)
-                .build();
-    }
+
     @Transactional
     public void addFavoriteToPost(FavoriteDto favoriteDto) {
         Long postId = favoriteDto.getPostId();
         String userId = favoriteDto.getUserId();
-        Post post = postRepository.findById(postId).orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다."));
-        UserEntity user = userRepository.findByUserId(userId).orElseThrow(() -> new NotFoundException("사용자를 찾을수없습니다"));;
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException());
+        UserEntity user = userRepository.findByUserId(userId).orElseThrow(() -> new UserNotFoundException());
 
         favoriteRepository.findByFavoritePostAndFavoriteUser(post, user).ifPresentOrElse(
-                (favorite)-> new NotFoundException("User already liked this post"),
+                (favorite)-> new DataAlreadyExistException(),
                 ()->{
                     Favorite favorite=new Favorite();
                     favoriteRepository.save(favorite);
@@ -54,10 +42,10 @@ public class FavoriteService {
     public void removeFavoriteToPost(FavoriteDto favoriteDto) {
         Long postId = favoriteDto.getPostId();
         String userId = favoriteDto.getUserId();
-        Post post = postRepository.findById(postId).orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다."));
-        UserEntity user = userRepository.findByUserId(userId).orElseThrow(() -> new NotFoundException("사용자를 찾을수없습니다"));
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException());
+        UserEntity user = userRepository.findByUserId(userId).orElseThrow(() -> new UserNotFoundException());
         Favorite favorite = favoriteRepository.findByFavoritePostAndFavoriteUser(post, user)
-                .orElseThrow(() -> new NotFoundException("User has not liked this pos"));
+                .orElseThrow(() -> new DataAlreadyRemoveException());
         post.removeFavorite(favorite);
         favoriteRepository.delete(favorite);
     }
